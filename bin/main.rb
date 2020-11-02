c25c077a79606bb42bd697d8c5dbc34c05fe2cb7
mke2111 <roymke134@gmail.com>
2020-11-02 16:40:26 +0300

    added the welcome method

--- bin/main.rb
+++ bin/main.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 require 'telegram/bot'
-# require_relative '../bin/config'
+require_relative '../config'
 require_relative '../lib/bot'
 require_relative '../lib/set_up'
 
-puts 'Welcome, my name is KAREN, I"m a telegram bot here to fulfil none of your interests but mine!'
+puts 'Welcome, my name is KAREN, I"m a telegram bot here to fulfil none of your interests but mine, Because I care about you.'
 # puts "KAREN has loaded #{dots}"
 puts 'https://web.telegram.org/resolve?domain=botKARENbot#/im?p=@botKARENbot'
 
@@ -14,7 +14,9 @@ Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hi #{message.from.first_name}, #{karen.welcome}")
+      bot.api.send_message(
+        chat_id: message.chat.id,
+        text: "#{message.from.first_name}, #{karen.welcome}")
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
     end
--- lib/bot.rb
+++ lib/bot.rb
@@ -1,9 +1,20 @@
-require 'telegram/bot'
-# require_relative '../config'
+
 
 class Karen
+  def initialize
+    @start = Time.now.to_i
+    info 
+  end
+
   def welcome
-    'Did you talk to mum before you recalled me? You should be with me more often.'
+    "You should be with me more often, I make your life much healthier and better. It is #{Time.now}, take a glass of water to give you life NOW!"
+  end
+
+  def info
+    "You finally decide to run me, I'll kill you with water"
+  end
+  
+  def bye
+
   end
 end
-
